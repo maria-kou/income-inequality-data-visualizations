ab2a0e64c0e30b99e8fcf53bfa6d7e1434793750
tbarous-edu <tasos.barous@outlook.com>
2020-05-26 14:18:26 +0300

    vue

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'app/dist')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
+  .get('/', (req, res) => {
+  	// res.render('index')
+  	res.sendFile(__dirname + '/app/dist/index.html');
+   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
